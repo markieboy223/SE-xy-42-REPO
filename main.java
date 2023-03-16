832e30c72dda6c39dd9eec2e2fee5827c2e9e02e
Misha <22129707@student.hhs.nl>
2023-03-16 10:38:18 +0100

    twat!

--- main.java
+++ main.java
@@ -1,7 +1,40 @@
-class optie {
+import java.util.ArrayList;
+
+class Optie{
+    private String naam;
+    private String omschrijving;
+    private Double prijs;
+    private Boolean isEssentieel;
+}
+
+class Klant{
+    private String type;
+    private String naam;
+    private String adres;
+    private int kvkNummer;
+    private boolean isOverheid;
+}
+
+class Korting{
+    private String type;
+    private Double percentage;
+}
+
+class Plezierjacht{
+    ArrayList <Optie> opties = new ArrayList<Optie>();
+    String naam;
+    String merk;
+    Double prijs;
+    Double lengte;
+    Double gewicht;
 
 }
 
 public class main {
+    public static String test = "boot";
+    public static void main(String[] args) {
+        System.out.println(test);
+
+    }
 
 }
