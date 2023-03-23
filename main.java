a216307040670ed7794d66042a0153d9850694b3
markieboy223 <markvanderburg01@gmail.com>
2023-03-23 09:18:23 +0100

    out

--- main.java
+++ main.java
@@ -4,10 +4,17 @@ class Plezierjacht{
     ArrayList <Optie> opties = new ArrayList<Optie>();
     private String naam;
     private String merk;
-    private Double prijs;
-    private Double lengte;
-    private Double gewicht;
+    private double prijs;
+    private double lengte;
+    private double gewicht;
 
+    public Plezierjacht(String naam, String merk, double prijs, double lengte, double gewicht){
+        this.naam = naam;
+        this.merk = merk;
+        this.prijs = prijs;
+        this.lengte = lengte;
+        this.gewicht = gewicht;
+    }
 }
 
 class Optie{
@@ -38,6 +45,9 @@ public class main {
         System.out.println("Optie 1: Een offerte aanmaken.");
         System.out.println("Optie 2: Aanpassingen maken aan ShipFlex.");
         //Hier met een scanner kijken of er 1 of 2 wordt getypt
+
+        Plezierjacht plezierjacht = new Plezierjacht("bootje", "speeder", 25000.00, 7.5, 2000);
+        System.out.println(plezierjacht);
     }
 
 }
---
+++ out/production/SE-xy-42-REPO/README.md
@@ -0,0 +1,2 @@
+# SE-xy-42-REPO
+Project voor bedrijf 42, het maken van een facturatie applicatie voor de botenindustrie.
