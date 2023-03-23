509ff0b30832ebdc445e7c4a3b538f9663422cf1
Misha <22129707@student.hhs.nl>
2023-03-23 11:17:37 +0100

    Misha

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Optie{
     private String naam;
@@ -7,17 +8,104 @@ class Optie{
     private Boolean isEssentieel;
 }
 
-class Klant{
-    private String type;
-    private String naam;
-    private String adres;
-    private int kvkNummer;
-    private boolean isOverheid;
+class Klant extends Korting{
+    private String naam = "";
+    private String adres = "";
+    private int kvkNummer = 0;
+    private boolean isOverheid = false;
+
+    public Klant(String type, Double percentage) {
+        super(type, percentage);
+    }
+    public void type1(){ //Hier check ik of de klant particulier, bedrijf od overheid is
+        if (kvkNummer == 0 && isOverheid){
+            type = "particulier";
+            percentage = 0.0;
+        }
+        else if (!isOverheid) {
+            type = "overheid";
+            percentage = 15.0;
+        }
+        else if (kvkNummer != 0) {
+            type = "bedrijf";
+            percentage = 10.0;
+        }
+    }
+    public void klantNaam(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geef uw naam:");
+        String naam = scanner.next();
+        setNaam(naam);
+        System.out.println("Geef uw adres:");
+        String adres = scanner.next();
+        setAdres(adres);
+        System.out.println("Heeft u een kvkNummer J of N:");
+        String kvkNummerJN = scanner.next();
+        if (kvkNummerJN.equals("J")){ //Hier checkt de if of er J is ingevoerd en daar vraagt hij dan om het kvkNummer
+            System.out.println("Geef uw kvkNummer");
+            int kvkNummer = scanner.nextInt();
+            setKvkNummer(kvkNummer);
+        }
+        else if (kvkNummerJN.equals("N")){// Als er N wordt ingevoerd en dan gaat hij door naar of het een overheid is
+            System.out.println("Bent u van de overheid? J of N");
+            String overheid = scanner.next();
+            if(overheid.equals("J")){ //Als overheid J is wordt hij true en wordt hij geset
+                boolean overheid2 = true;
+                setOverheid(overheid2);
+            }
+            else if (overheid.equals("N")) { //Als overheid N is wordt hij false en word hij geset
+                boolean overheid2 = false;
+                setOverheid(overheid2);
+            }
+        }
+    }
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+    public boolean isOverheid() {
+        return isOverheid;
+    }
+    public int getKvkNummer() {
+        return kvkNummer;
+    }
+    public String getAdres() {
+        return adres;
+    }
+    public String getNaam() {
+        return naam;
+    }
+    public void setOverheid(boolean overheid) {
+        this.isOverheid = overheid;
+    }
+    public void setKvkNummer(int kvkNummer) {
+        this.kvkNummer = kvkNummer;
+    }
+
 }
 
 class Korting{
-    private String type;
-    private Double percentage;
+    protected String type = "";
+    protected Double percentage = 0.0;
+
+    public Korting(String type, Double percentage) {
+        this.type = type;
+        this.percentage = percentage;
+    }
+
+    public Korting(String type) {
+        this.type = type;
+    }
+
+    public void setPercentage(Double percentage) {
+        this.percentage = percentage;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
 
 class Plezierjacht{
@@ -31,10 +119,9 @@ class Plezierjacht{
 }
 
 public class main {
-    public static String test = "boot";
     public static void main(String[] args) {
-        System.out.println(test);
-
+        Klant klant = new Klant("", 0.0);
+        klant.klantNaam();
     }
 
 }
